a8f6de7977774b98d76915492566f49ad59cdd6c
esteban056 <estebang056@gmail.com>
2017-11-21 20:23:29 -0600

    Se agrego flask numero de puerto y analisis de sentimientos

--- servicios/sv_clasification.py
+++ servicios/sv_clasification.py
@@ -1,75 +1,45 @@
 # -*- coding: utf-8 -*-
 from afinn import Afinn
 import sqlite3
-
+from flask import Flask, abort, render_template, request
+import json
+import os
 
 afinn = Afinn(language = 'en')
-
-query = 'superman'
-
-conn = sqlite3.connect('tweets.db')
-cursor = conn.cursor()
-cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
-print cursor.fetchall()
-result = cursor.execute("SELECT * FROM Tweets WHERE query='"+query+"'")
-for row in result:
-	print row
-"""
-#Devuelve contadores de la distribución de polaridades, es decir, cuántos tweets positivos, neutrales y negativos son.
-def parse_response(json_response):
-    negative_tweets, positive_tweets, neutral_tweets = 0, 0, 0
-    for j in json_response["data"]:
-        if int(j["polarity"]) == 0:
-            neutral_tweets += 1
-        elif int(j["polarity"]) > 0:
-            positive_tweets += 1
-        elif int(j["polarity"]) < 0:
-            negative_tweets += 1
-    return negative_tweets, positive_tweets, neutral_tweets
+app = Flask(__name__)
 
 
-@app.route("/api/v1/analysis", methods=['GET'])
-def analysis():
 
-    TWEET_TOPIC = request.args.get("t")
 
-    if TWEET_TOPIC is not None:
-        afinn = Afinn()
-        tweets = []    
+def analize(text):
+	return 'positive' if afinn.score(text) > 0 else 'negative'
 
-        #La función connect nos permite conectarnos a la base de datos.
-        conn = sqlite3.connect('tweets.db')
-        #La función cursor() creamos un objeto de tipo cursor para poder ejecutar comandos SQL.
-        c = conn.cursor()
+@app.route("/api/v3/sentiment", methods=['GET'])
+def get_sentiment():
+	query = request.args.get('t')
+	conn = sqlite3.connect('tweets.db')
+	cursor = conn.cursor()
+	cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+	print cursor.fetchall()
+	result = cursor.execute("SELECT * FROM Tweets WHERE query='"+query+"'")
+	data = {}
+	comments = []
 
-        #Obtenemos los comentarios guardados en la base de datos de la película buscada.
-        for t in c.execute("SELECT * FROM Tweets WHERE query='{0}'".format(TWEET_TOPIC)):
-            #Para no realizar doble procesamiento, desde el momento en obtener desde la base de datos se envían
-            #a la librería que realizará la clasificación por análisis de sentimientos del texto de los tweets.
-            tweet = {"text": str(t[1]),
-                     "query": str(t[2]),
-                     "polarity": afinn.score(str(t[1]))}
+	for row in result:
+		data['id']=row[0]
+		data['text']=row[1]
+		data['query']= row[2]
+		data['polarity'] = analize(row[1])
+		comments.append(data)
+		data = {}
 
-            tweets.append(tweet)
+	conn.close()
+	return json.dumps(comments)
 
-        #Cerramos la conexión con la base de datos.
-        conn.close
-
-        result = {"data": tweets}
-
-        negative_tweets, positive_tweets, neutral_tweets = parse_response(result)
-
-        classification_results = {}
-
-        #Se da forma al JSON de respuesta que recibirá la GUI para mostrar el resultado del analisis de sentimientos.
-        classification_results['pos'] = positive_tweets
-        classification_results['neg'] = negative_tweets
-        classification_results['ntl'] = neutral_tweets
-        classification_results['total'] = negative_tweets + positive_tweets + neutral_tweets
-
-        return json.dumps(classification_results)
-
-    else:
-        # Se devuelve un error 400 para indicar que el servicio no puede funcionar sin parámetro
-        abort(400)
-"""
+if __name__ == '__main__':
+    # Se define el puerto del sistema operativo que utilizará el servicio
+    port = int(os.environ.get('PORT', 8002))
+    # Se habilita la opción de 'debug' para visualizar los errores
+    app.debug = True
+    # Se ejecuta el servicio definiendo el host '0.0.0.0' para que se pueda acceder desde cualquier IP
+    app.run(host='0.0.0.0', port=port)
